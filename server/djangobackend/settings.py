67720b4b358d0079c656b3c262217bef46d38a33
abdelaziz1997 <abdelaziz.elhassouni97@gmail.com>
2021-08-18 03:08:51 +0000

    Temporary Changes

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+MEDIA_ROOT = os.path.join(os.path.join(BASE_DIR, 'static'), 'media')
 MEDIA_URL = '/media/'
