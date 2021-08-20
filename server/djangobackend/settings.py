75287fc67525960e0a5bd927ed1da5ca6b980f75
abdelaziz1997 <abdelaziz.elhassouni97@gmail.com>
2021-08-20 05:14:29 +0000

    Module 1 Tasks Done

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost","aelh.eu-gb.cf.appdomain.cloud"]
 
 
 # Application definition
@@ -123,7 +123,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'var', 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 MEDIA_ROOT = os.path.join(os.path.join(BASE_DIR, 'static'), 'media')
 MEDIA_URL = '/media/'
